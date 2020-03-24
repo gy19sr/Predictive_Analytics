15bcd7b5f607f480928627fb99ed2f70c79d8df4
S. Ross <56163785+gy19sr@users.noreply.github.com>
2020-03-24 14:59:37 -0400

    categorical to numbers

--- predictive.py
+++ predictive.py
@@ -12,24 +12,23 @@ from sklearn.model_selection import train_test_split
 from sklearn.impute import SimpleImputer
 import matplotlib.pyplot as plt
 import csv
-
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import LabelEncoder
 
 #import data
 data = pd.read_csv("C:/Users/stuar/OneDrive/Documents/onlinecourses/Linkedin/predictive_analytics_through_python/Predictive_Analytics/Ex_Files_Python_Predictive_Analytics/Ex_Files_Python_Predictive_Analytics/Exercise Files/Datasets/insurance.csv")
 
 print(data.head(15))
 
-#### dealing with NA's
+######## dealing with NA's #################
 
 #print if a value is NA (or in pythonNAN) null
 #data.isnull()
 #print(data.isnull())
 
-
 data.isnull().sum().sum()
 print(data.isnull().sum())
 print("total nulls =",data.isnull().sum().sum()) #all rows and columns combine
-
 
 #fill missing values with .... (mean or median or most_frequent)
 
@@ -42,8 +41,6 @@ count_nan = data.isnull().sum() # the number of missing values for every column
 print(count_nan[count_nan > 0])
 print(data.head(15))
 print(data.isnull().sum())
-
-
 
 
 #if want to remove NAs (rows)
@@ -60,7 +57,6 @@ print(data.head(15))
 print(data.isnull().sum())
 """
 
-
 #if want to remove NA columns
 """
 data = pd.read_csv("C:/Users/stuar/OneDrive/Documents/onlinecourses/Linkedin/predictive_analytics_through_python/Predictive_Analytics/Ex_Files_Python_Predictive_Analytics/Ex_Files_Python_Predictive_Analytics/Exercise Files/Datasets/insurance.csv")
@@ -68,3 +64,43 @@ data = pd.read_csv("C:/Users/stuar/OneDrive/Documents/onlinecourses/Linkedin/pre
 data.drop('bmi', axis = 1, inplace = True)
 print(data.isnull().sum())
 """
+
+################## Converting Categorical variables to numbers ############################
+
+####label encoding - two distinct values (so binomial)
+
+#id the variables
+sex = data.iloc[:,1:2].values
+smoker = data.iloc[:,4:5].values
+
+#le (label encoder) for smoker
+le = LabelEncoder()
+smoker[:,0] = le.fit_transform(smoker[:,0])
+smoker = pd.DataFrame(smoker)
+smoker.columns = ['smoker']
+le_smoker_mapping = dict(zip(le.classes_, le.transform(le.classes_)))
+print("sklearn label encoder results for smoker ")
+print(le_smoker_mapping)
+print(smoker[:10])
+
+## le (label encoder) for sex
+le = LabelEncoder()
+sex[:,0] = le.fit_transform(sex[:,0])
+sex = pd.DataFrame(sex)
+sex.columns = ['sex']
+le_sex_mapping = dict(zip(le.classes_, le.transform(le.classes_)))
+print("Sklearn label encoder results for sex:") 
+print(le_sex_mapping)
+print(sex[:10])
+
+
+####One hot encoding - three or more distinct values 
+
+#new columns for each option 1 is present 0 is not
+
+region = data.iloc[:,5:6].values
+
+
+
+
+
