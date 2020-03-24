171fbc612d1cae4bf6a9633d754b19f4a31ac9c5
S. Ross <56163785+gy19sr@users.noreply.github.com>
2020-03-24 14:26:38 -0400

    Na's

--- predictive.py
+++ predictive.py
@@ -19,20 +19,52 @@ data = pd.read_csv("C:/Users/stuar/OneDrive/Documents/onlinecourses/Linkedin/pre
 
 print(data.head(15))
 
+#### dealing with NA's
 
 #print if a value is NA (or in pythonNAN) null
-data.isnull()
-print(data.isnull())
+#data.isnull()
+#print(data.isnull())
+
 
 data.isnull().sum().sum()
 print(data.isnull().sum())
 print("total nulls =",data.isnull().sum().sum()) #all rows and columns combine
 
 
-#fill missing values with .... (mean or median or mode)
+#fill missing values with .... (mean or median or most_frequent)
 
-#check if still NA's
+data = pd.read_csv("C:/Users/stuar/OneDrive/Documents/onlinecourses/Linkedin/predictive_analytics_through_python/Predictive_Analytics/Ex_Files_Python_Predictive_Analytics/Ex_Files_Python_Predictive_Analytics/Exercise Files/Datasets/insurance.csv")
+imputer = SimpleImputer(strategy='mean')#develope a way to calc mean
+imputer.fit(data['bmi'].values.reshape(-1, 1)) #prepare bmi column
+data['bmi'] = imputer.transform(data['bmi'].values.reshape(-1, 1)) #mean of bmi and replace
+#check how many values are missing (NaN) - after we filled in the NaN
+count_nan = data.isnull().sum() # the number of missing values for every column
+print(count_nan[count_nan > 0])
+print(data.head(15))
+print(data.isnull().sum())
 
-#remove NA rows
 
-#remove NA columns
+
+
+#if want to remove NAs (rows)
+"""
+data = pd.read_csv("C:/Users/stuar/OneDrive/Documents/onlinecourses/Linkedin/predictive_analytics_through_python/Predictive_Analytics/Ex_Files_Python_Predictive_Analytics/Ex_Files_Python_Predictive_Analytics/Exercise Files/Datasets/insurance.csv")
+ # reloading fresh dataset for option 0
+#option1 for dropping NAN
+data.dropna(inplace=True)
+data.reset_index(drop=True, inplace=True)
+#check how many values are missing (NaN) - after we filled in the NaN
+count_nan = data.isnull().sum() # the number of missing values for every column
+print(count_nan[count_nan > 0])
+print(data.head(15))
+print(data.isnull().sum())
+"""
+
+
+#if want to remove NA columns
+"""
+data = pd.read_csv("C:/Users/stuar/OneDrive/Documents/onlinecourses/Linkedin/predictive_analytics_through_python/Predictive_Analytics/Ex_Files_Python_Predictive_Analytics/Ex_Files_Python_Predictive_Analytics/Exercise Files/Datasets/insurance.csv")
+ # reloading fresh dataset for option 0
+data.drop('bmi', axis = 1, inplace = True)
+print(data.isnull().sum())
+"""
