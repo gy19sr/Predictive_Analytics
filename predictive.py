5fd51e663a8c516f11a43510171b48dd1d961362
S. Ross <56163785+gy19sr@users.noreply.github.com>
2020-03-24 09:13:45 -0400

    initial

---
+++ predictive.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import keras
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.impute import SimpleImputer
+import matplotlib.pyplot as plt
+import csv
+
