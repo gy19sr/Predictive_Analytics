6fb3ebbf96b64f6207ee4098d7120f8821e5dcc7
S. Ross <56163785+gy19sr@users.noreply.github.com>
2020-03-24 11:52:25 -0400

    na's

--- predictive.py
+++ predictive.py
@@ -4,7 +4,7 @@ Spyder Editor
 
 This is a temporary script file.
 """
-
+import os
 import keras
 import pandas as pd
 import numpy as np
@@ -13,3 +13,26 @@ from sklearn.impute import SimpleImputer
 import matplotlib.pyplot as plt
 import csv
 
+
+#import data
+data = pd.read_csv("C:/Users/stuar/OneDrive/Documents/onlinecourses/Linkedin/predictive_analytics_through_python/Predictive_Analytics/Ex_Files_Python_Predictive_Analytics/Ex_Files_Python_Predictive_Analytics/Exercise Files/Datasets/insurance.csv")
+
+print(data.head(15))
+
+
+#print if a value is NA (or in pythonNAN) null
+data.isnull()
+print(data.isnull())
+
+data.isnull().sum().sum()
+print(data.isnull().sum())
+print("total nulls =",data.isnull().sum().sum()) #all rows and columns combine
+
+
+#fill missing values with .... (mean or median or mode)
+
+#check if still NA's
+
+#remove NA rows
+
+#remove NA columns
